49ede301e29613b45fc918c818f5677a84e3a319
MoryakSPb <31323043+MoryakSPb@users.noreply.github.com>
2021-08-28 22:39:04 +0300

    Hotfix thread blocking

--- Server/Discord/Enums/DiscordPermissionsFlags.cs
+++ Server/Discord/Enums/DiscordPermissionsFlags.cs
@@ -7,11 +7,11 @@ namespace AndNetwork.Server.Discord.Enums
     {
         None = 0,
 
-        View = 2098176,
-        Read = 3212288,
-        Write = 176130477632UL,
-        Priority = 244866862912UL,
-        Moderator = 262059323328UL,
+        View = 2098176UL,
+        Read = 3212288UL,
+        Write = 313569431104UL,
+        Priority = 382305816384UL,
+        Moderator = 399498276800UL,
 
         All = ulong.MaxValue,
     }
